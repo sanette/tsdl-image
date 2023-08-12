a214c57d2170e731bf469e08fb2b28410c9e553c
linsy king <linsy_king@sjtu.edu.cn>
2023-08-12 11:15:05 +0800

    keep functions for 2.0.2

--- src/tsdl_image.mli
+++ src/tsdl_image.mli
@@ -25,8 +25,6 @@ module Image : sig
     val png : t
     val tif : t
     val webp : t
-    val jxl : t
-    val avif : t
   end
 
   val init : Init.t -> Init.t
@@ -36,19 +34,16 @@ module Image : sig
   (** {{:https://wiki.libsdl.org/SDL_image/IMG_Quit} IMG_Quit} *)
 
   type format =
-    | Avif
     | Ico
     | Cur
     | Bmp
     | Gif
     | Jpg
-    | Jxl
     | Lbm
     | Pcx
     | Png
     | Pnm
     | Svg
-    | Qoi
     | Tif
     | Xcf
     | Xpm
@@ -87,10 +82,6 @@ module Image : sig
 
   val load_format_rw : format -> Tsdl.Sdl.rw_ops -> Tsdl.Sdl.surface result
 
-  val load_sized_svg_rw :
-    Tsdl.Sdl.rw_ops -> int -> int -> Tsdl.Sdl.surface result
-  (** {{:https://wiki.libsdl.org/SDL2_image/IMG_LoadSizedSVG_RW}
-        IMG_LoadSizedSVG_RW} *)
 
   val read_xpm_from_array : string -> Tsdl.Sdl.surface result
   (** {{:https://wiki.libsdl.org/SDL_image/IMG_ReadXPMFromArray}
